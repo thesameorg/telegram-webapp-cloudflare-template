a7c769b8e998c61d294ea93969140a7043c616e1
Garuda <106117111+garootman@users.noreply.github.com>
2025-09-29 00:05:28 +0700

    commands update

--- scripts/install-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install all dependencies
-echo "Installing all dependencies"
-echo "Installing backend dependencies..."
-cd backend && npm ci
-echo "Installing frontend dependencies..."
-cd ../frontend && npm ci
-echo "All dependencies installed"
