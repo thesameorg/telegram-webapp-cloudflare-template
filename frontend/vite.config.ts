057189f4f8102e91382fa72cd8f7b01d61d28792
Garuda <106117111+garootman@users.noreply.github.com>
2025-10-07 19:17:17 +0700

    vars for TELEGRAM_BOT_USERNAME

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  envDir: "../", // Load .env from root directory
   server: {
     port: 3000,
     host: true,
